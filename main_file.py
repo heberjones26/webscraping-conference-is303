e47dca16506990b10645077c0e3a5a5fc1042523
heberbjones <admin@heberjones.com>
2026-04-13 13:48:18 -0600

    commit

--- main_file.py
+++ main_file.py
@@ -117,7 +117,7 @@ def main():
         #   member2.scrape_and_save(talk_urls, engine)
         print("Talk URLs ready — passing to Member 2's scraper.")
         # TODO: call Member 2's scrape_and_save() here
-
+        scrape_and_save(talk_ursl)
 
  
         print("You've saved the scraped data to your postgres database.")
@@ -141,10 +141,36 @@ if __name__ == "__main__":
 
 # Scraping function - Heber (Member 2)
 
-def scrape_and_save(talk_ursl, engine) :
+def scrape_and_save(talk_ursl) :
 
     # Scrape data from every URL in the list prodived
     for url in talk_ursl :
+        # Create a dictionary to track references for each book of scripture:
+        standard_works_dict = {'Speaker_Name' : '', 'Talk_Name' : '', 'Kicker' : '', 
+                               'Matthew': 0, 'Mark': 0, 'Luke': 0, 'John': 0, 'Acts': 0, 
+                               'Romans': 0, '1 Corinthians': 0, '2 Corinthians': 0, 
+                               'Galatians': 0, 'Ephesians': 0, 'Philippians': 0, 
+                               'Colossians': 0, '1 Thessalonians': 0, '2 Thessalonians': 0, 
+                               '1 Timothy': 0, '2 Timothy': 0, 'Titus': 0, 'Philemon': 0, 
+                               'Hebrews': 0, 'James': 0, '1 Peter': 0, '2 Peter': 0, 
+                               '1 John': 0, '2 John': 0, '3 John': 0, 'Jude': 0, 
+                               'Revelation': 0, 'Genesis': 0, 'Exodus': 0, 'Leviticus': 0, 
+                               'Numbers': 0, 'Deuteronomy': 0, 'Joshua': 0, 'Judges': 0, 
+                               'Ruth': 0, '1 Samuel': 0, '2 Samuel': 0, '1 Kings': 0, 
+                               '2 Kings': 0, '1 Chronicles': 0, '2 Chronicles': 0, 'Ezra': 0, 
+                               'Nehemiah': 0, 'Esther': 0, 'Job': 0, 'Psalm': 0, 'Proverbs': 0, 
+                               'Ecclesiastes': 0, 'Song of Solomon': 0, 'Isaiah': 0, 
+                               'Jeremiah': 0, 'Lamentations': 0, 'Ezekiel': 0, 'Daniel': 0, 
+                               'Hosea': 0, 'Joel': 0, 'Amos': 0, 'Obadiah': 0, 'Jonah': 0, 
+                               'Micah': 0, 'Nahum': 0, 'Habakkuk': 0, 'Zephaniah': 0, 
+                               'Haggai': 0, 'Zechariah': 0, 'Malachi': 0, '1 Nephi': 0, 
+                               '2 Nephi': 0, 'Jacob': 0, 'Enos': 0, 'Jarom': 0, 'Omni': 0, 
+                               'Words of Mormon': 0, 'Mosiah': 0, 'Alma': 0, 'Helaman': 0, 
+                               '3 Nephi': 0, '4 Nephi': 0, 'Mormon': 0, 'Ether': 0, 'Moroni': 0, 
+                               'Doctrine and Covenants': 0, 'Moses': 0, 'Abraham': 0, 
+                               'Joseph Smith—Matthew': 0, 'Joseph Smith—History': 0, 
+                               'Articles of Faith': 0}
+
         # Convert the URL into scrapable data
         oResponse = requests.get(url)
         soup = BeautifulSoup(oResponse.text, "html.parser")
@@ -153,4 +179,14 @@ def scrape_and_save(talk_ursl, engine) :
         # speaker name, title, and kicker
         # Footnotes, and update scripture reference count
 
+        header_section = soup.find("header")
+        speaker_name = header_section.find("p", class_="author-name")
+        title = header_section.find("h1")
+        kicker = header_section.find("p", class_="kicker")
 
+        footnotes_section = soup.find('footer', attrs={'class' : 'notes'})
+        for books in standard_works_dict :
+            iBookReference = footnotes_section.count(books)
+            standard_works_dict[books] = iBookReference
+
+        print(speaker_name, title, kicker, standard_works_dict)
